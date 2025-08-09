25f26e0e042c6a474447dfff3864d733cd7fa490
Stephane-Maissa <155585813+Stephane-Maissa@users.noreply.github.com>
2025-08-09 18:29:53 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+/** @type {import('next').NextConfig} */
+export default {
+  reactStrictMode: true,
+
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  async rewrites() {
+    return [{ source: '/m/:id', destination: '/m/index.html' }];
+  },
+  async headers() { /* ... tes headers ... */ },
+};
 
 export default nextConfig;
